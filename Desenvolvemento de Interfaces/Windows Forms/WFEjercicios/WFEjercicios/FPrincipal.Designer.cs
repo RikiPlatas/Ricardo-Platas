2b8d7072d8ccae871ad324762b2b1442679aef88
RikiPlatas <rikiplatas11@gmail.com>
2022-09-22 12:21:21 +0200

    Ejercicios Windows Forms

---
+++ Desenvolvemento de Interfaces/Windows Forms/WFEjercicios/WFEjercicios/FPrincipal.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace WFEjercicios
+{
+    partial class FPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1184, 116);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Location = new System.Drawing.Point(0, 0);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 100);
+            this.panel4.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Location = new System.Drawing.Point(0, 0);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(200, 100);
+            this.panel3.TabIndex = 0;
+            // 
+            // panel5
+            // 
+            this.panel5.BackColor = System.Drawing.Color.GreenYellow;
+            this.panel5.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel5.Location = new System.Drawing.Point(0, 0);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(1040, 100);
+            this.panel5.TabIndex = 0;
+            this.panel5.Paint += new System.Windows.Forms.PaintEventHandler(this.panel5_Paint);
+            // 
+            // panel6
+            // 
+            this.panel6.BackColor = System.Drawing.Color.Gold;
+            this.panel6.Controls.Add(this.button1);
+            this.panel6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel6.Location = new System.Drawing.Point(0, 396);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(1040, 100);
+            this.panel6.TabIndex = 1;
+            this.panel6.Paint += new System.Windows.Forms.PaintEventHandler(this.panel6_Paint_1);
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.SlateGray;
+            this.panel7.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel7.Location = new System.Drawing.Point(0, 100);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(238, 296);
+            this.panel7.TabIndex = 2;
+            this.panel7.Paint += new System.Windows.Forms.PaintEventHandler(this.panel7_Paint);
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.Silver;
+            this.panel8.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel8.Location = new System.Drawing.Point(238, 100);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(802, 296);
+            this.panel8.TabIndex = 3;
+            this.panel8.Paint += new System.Windows.Forms.PaintEventHandler(this.panel8_Paint);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(840, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(188, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FPrincipal
+            // 
+            this.ClientSize = new System.Drawing.Size(1040, 496);
+            this.Controls.Add(this.panel8);
+            this.Controls.Add(this.panel7);
+            this.Controls.Add(this.panel6);
+            this.Controls.Add(this.panel5);
+            this.Name = "FPrincipal";
+            this.Load += new System.EventHandler(this.FPrincipal_Load);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel4;
+        private Panel panel2;
+        private Panel panel3;
+        private Panel panel5;
+        private Panel panel6;
+        private Panel panel7;
+        private Panel panel8;
+        private Button button1;
+    }
+}
---
+++ Desenvolvemento de Interfaces/Windows Forms/WFEjercicios/WFEjercicios/FPrincipal.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WFEjercicios
+{
+    public partial class FPrincipal : Form
+    {
+        public FPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void FPrincipal_Load(object sender, EventArgs e)
+        {
+
+            this.BackColor = Color.LimeGreen;
+            this.Text = "Este es el titulo en ejecucion";
+            // this.agregarBotonDinamico();
+            this.agregarLabelDinamica();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+          
+        }
+
+        private void panel6_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel5_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel7_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel8_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel6_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void agregarLabelDinamica()
+        {
+            Label lblDinamica = new Label();
+            lblDinamica.Name = "LblDinamica";
+            lblDinamica.Text = "Ejercicios Windows Forms";
+            lblDinamica.Location = new System.Drawing.Point(25, 25);
+            lblDinamica.Size = new System.Drawing.Size(300, 250);
+            Font miFuente = new Font("Arial", 20);
+            lblDinamica.Font = miFuente;
+            this.panel5.Controls.Add(lblDinamica);
+            
+        }
+        /*
+private void agregarBotonDinamico()
+{
+Button btnDinamico = new Button();
+btnDinamico.Name = "btnDinamico";
+btnDinamico.Text = "Mangui Boton";
+btnDinamico.Location = new System.Drawing.Point(25, 25);
+btnDinamico.Size = new System.Drawing.Size(100, 25);
+this.Controls.Add(btnDinamico);
+}
+*/
+
+
+    }
+}
