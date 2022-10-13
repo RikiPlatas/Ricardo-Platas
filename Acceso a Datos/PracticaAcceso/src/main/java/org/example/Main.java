51b718130bf2500beef6355670044ccaf7225129
RikiPlatas <rikiplatas11@gmail.com>
2022-10-13 13:04:01 +0200

    continucacion practica

--- Acceso a Datos/PracticaAcceso/src/main/java/org/example/Main.java
+++ Acceso a Datos/PracticaAcceso/src/main/java/org/example/Main.java
@@ -5,14 +5,13 @@ import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    static Scanner teclado =new Scanner(System.in);
+    static Scanner teclado = new Scanner(System.in);
     static AventuraJABX aventura = new AventuraJABX();
 
-    public static void main(String[] args) throws JAXBException, IOException {
+    public static void main(String[] args) throws JAXBException, IOException, InterruptedException {
 
         System.out.println("                                                 /===-_---~~~~~~~~~------____\n" +
                 "                                                |===-~___                _,-'\n" +
@@ -44,6 +43,8 @@ public class Main {
                 "    '   '  `\n" +
                 "\n" +
                 "-----------------------------------------------------------------------------------\n");
+
+        Thread.sleep(5000);
         System.out.println("\n" +
                 "     _                             _                              \n" +
                 "    / \\    __   __   ___   _ __   | |_   _   _   _ __    __ _     \n" +
@@ -59,7 +60,6 @@ public class Main {
                 "                         |___/                                    \n");
 
 
-
         realizarLecturaXML("C:\\Users\\FP\\Desktop\\Practica Acceso\\XMLPractica.xml");
 
         //realizarGuardadoXML(lista,"C:\\Users\\FP\\Desktop\\2 DAM\\Acceso a datos\\generadoXMLJABX.xml");
@@ -68,6 +68,8 @@ public class Main {
     private static AventuraJABX realizarLecturaXML(String ruta) throws JAXBException {
 
         int escena = 0;
+        int opcionELegida = 0;
+        int res = 0;
         boolean fin = false;
 
         File file = new File(ruta);
@@ -77,55 +79,64 @@ public class Main {
             Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
 
             aventura = (AventuraJABX) jaxbUnmarshaller.unmarshal(file);
-            //System.out.println(aventura.getEscenas().get(0).getTexto());
+
+            while (!fin) {
+
+                EscenaJABX escenaActual = imprimirEscena(escena);
+
+                do {
+                    try {
+                        opcionELegida = teclado.nextInt();
+                        if (opcionELegida <= 0 || opcionELegida > 3) {
+                            System.out.println("Introduce una opcion valida");
+                        }
+
+                    } catch (Exception e) {
+                        System.out.println("Has escogido una opcion no valida");
+                        break;
+                    }
+                } while (opcionELegida <= 0 || opcionELegida > 3);
 
 
-           while(!fin){
+                for (OpcionesJABX opcion : escenaActual.getOpciones()) {
+                    if (opcionELegida == opcion.getId()) {
+                        escena = opcion.getResultado();
+                    }
 
-               imprimirEscena(escena);
-
-               do{
-                   try{
-                       escena = teclado.nextInt();
-                       if (escena <=0 || escena >3 ){
-                           System.out.println("Introduce una opcion valida");
-                       }
-
-                   }catch (Exception e){
-                       System.out.println("Has escogido una opcion no valida");
-                       break;
-                   }
-               }while(escena <=0 || escena >3);
+                }
 
 
-
-            //fin = true;
-
-           }
+            }
 
 
-        } else {
-            System.out.println("No se ha podido encontrar el fichero indicado");
         }
-
-
         return aventura;
-
     }
 
-    private static void imprimirEscena(int cod){
 
+    private static EscenaJABX imprimirEscena(int cod) {
+
+
+        EscenaJABX test = null;
         //for each para recorres las escenas
-        for(EscenaJABX escena1: aventura.getEscenas()){
+        for (EscenaJABX escena1 : aventura.getEscenas()) {
             //if para coger la escena del codigo que te pasen
-            if(escena1.getCodigo() == cod){
+            if (escena1.getCodigo() == cod) {
                 System.out.println(escena1.getTexto());
                 //for each para recorrer las opciones de la escena que esta seleccionada
-                for(OpcionesJABX opcion1 : escena1.opciones ){
-                    System.out.println(opcion1.getId() +"-"+ opcion1.getTexto());
+                for (OpcionesJABX opcion1 : escena1.opciones) {
+                    System.out.println(opcion1.getId() + "-" + opcion1.getTexto());
+
                 }
+                test = escena1;
+
             }
+
+
         }
+        return test;
     }
 
+
 }
+
