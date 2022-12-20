c660c2a45df7b680023592bc252cf2d714137a84
RikiPlatas <rikiplatas11@gmail.com>
2022-12-20 14:22:53 +0100

    practica3

--- Acceso a Datos/NeoDatis/src/main/java/org/example/Main.java
+++ Acceso a Datos/NeoDatis/src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -8,28 +9,60 @@ public class Main {
 
         int cont = 0;
         int respuesta = 0;
+        int contAciertos = 0;
+        int contFallos = 0;
+
+        //para las preguntas en orden aleatorio
+        int random = 0;
+
 
         PreguntaDAO preguntaDAO = new PreguntaDAO();
 
+/*
+        for(int i = 0;i<100;i++){
+            random = (int)(Math.random()*preguntaDAO.listar().size()+0);
+            System.out.println(random);
+        }
 
-
+*/
 
 
         while(cont < preguntaDAO.listar().size())
         {
-            System.out.println(preguntaDAO.listar().get(cont));
-            cont++;
+            //preguntas en orden aleatorio
+            ArrayList<Integer> numerosRandom = new ArrayList();
+
+            do{
+                random = (int)(Math.random()*preguntaDAO.listar().size()+0);
+            }while (numerosRandom.ma)
+
+
+
+
+
+            System.out.println(preguntaDAO.listar().get(random).getTexto());
+
+            for (int i = 0; i<preguntaDAO.listar().get(cont).opcion.size();i++){
+                System.out.println(i+")"+preguntaDAO.listar().get(cont).getOpcion().get(i).getTexto());
+            }
 
             System.out.println("escribe la respuesta (0-3)");
             respuesta = teclado.nextInt();
 
             if(preguntaDAO.listar().get(cont).getOpcion().get(respuesta).isAcierto() == true){
                 System.out.println("Acierto");
+                contAciertos++;
             }else {
                 System.out.println("Fallo");
+                contFallos++;
             }
+            cont++;
+
         }
+
+        System.out.println("Has acertado "+contAciertos+" preguntas, y has fallado "+contFallos+" preguntas");
 
 
     }
+
 }
